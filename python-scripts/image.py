5b6a22f2876a7305c17822cae2cb2bd8498579c7
Mickey Fraser <the2crabs@gmail.com>
2026-01-12 22:31:59 -0600

    Working masks

--- python-scripts/image.py
+++ python-scripts/image.py
@@ -2,23 +2,49 @@ import skimage as ski
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import os
+
+#returns gray rgb image with red pixels white
+def isolate_red(hsv_image):
+    # -> (row,column)
+    h = hsv_image[:, :, 0]
+    s = hsv_image[:, :, 1]
+
+    # create array with boolean values
+    mask = ((h < 0.06) | (h > 0.94)) & (s > 0.5)
+
+    # create 2d array with
+    bw = np.zeros_like(h, dtype=np.uint8)
+
+    # index with boolean array and set true values to white
+    bw[mask] = 255
+    return ski.color.gray2rgb(bw)
+
+#returns gray rgb image with red pixels white
+def isolate_green(hsv_image):
+    # -> (row,column)
+    h = hsv_image[:, :, 0]
+    s = hsv_image[:, :, 1]
+
+    # create array with boolean values
+    mask = (h > 0.16) & (h < 0.5) & (s > 0.3)
+
+    # create 2d array with
+    bw = np.zeros_like(h, dtype=np.uint8)
+
+    # index with boolean array and set true values to white
+    bw[mask] = 255
+    return ski.color.gray2rgb(bw)
 
 start = time.time()
 
-img = ski.data.astronaut()
+img = ski.io.imread('../imgs/test-2.jpg')
+
+# (row, column, channel), 0.0 to 1.0
 img_hsv = ski.color.rgb2hsv(img)
 
-for y in range(img_hsv.shape[0]):
-    for x in range(img_hsv.shape[1]):
-        h = img_hsv[y, x][0] * 255
-        s = img_hsv[y, x][1] * 255
-        if 60 > h > 5 and s > 128:
-            img[y, x] = [255, 255,255]
-        else:
-            img[y, x] = [0,0,0]
-
-
-ski.io.imsave('image.jpg', img)
+ski.io.imsave('red.jpg', isolate_red(img_hsv))
+ski.io.imsave('green.jpg', isolate_green(img_hsv))
 
 stop = time.time()
 print(stop - start)
